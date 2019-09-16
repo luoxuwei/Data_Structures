04d409888182d3f6f2bf72554b1e84385cc684c0
luoxuwei <xuwei.luo@nio.com>
2019-09-16 16:06:22 +0800

    添加 两种queue实现的性能对比测试用例

--- Stack_Queue/src/Main.java
+++ Stack_Queue/src/Main.java
@@ -1,6 +1,22 @@
+import java.util.Random;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        int testCount = 100000;
+        System.out.println("array queue "+testQueue(new ArrayQueue<>(), testCount));
+        System.out.println("loop queue "+testQueue(new LoopQueue<>(), testCount));
+    }
+
+    private static double testQueue(Queue<Integer> queue, int count) {
+        long begin = System.nanoTime();
+        Random random = new Random();
+        for (int i=0; i<count; i++) {
+            queue.enqueue(random.nextInt(Integer.MAX_VALUE));
+        }
+        for (int i=0; i<count; i++) {
+            queue.dequeue();
+        }
+        return (System.nanoTime() - begin)/1000000000.0;
     }
 }
