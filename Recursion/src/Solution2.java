04ca49925dbc17e083ca17677e5635bae00dc2cb
罗旭维 <luoxuwei@luoxuweideMacBook-Pro.local>
2019-09-17 22:33:45 +0800

    给链表添加dummy head 统一处理遍历逻辑，不用特别处理头节点

---
+++ Recursion/src/Solution2.java
@@ -0,0 +1,25 @@
+//https://leetcode-cn.com/problems/remove-linked-list-elements/submissions/
+public class Solution2 {
+    public ListNode removeElements(ListNode head, int val) {
+        ListNode dummyHead = new ListNode(-1);
+        dummyHead.next = head;
+        ListNode prv = dummyHead;
+        while (prv != null && prv.next != null) {
+
+            if (prv.next.val == val) {
+                prv.next = prv.next.next;
+            } else {
+                prv = prv.next;
+            }
+
+        }
+        return head;
+    }
+
+    public static void main(String[] a) {
+        ListNode test = new ListNode(new int[]{1,2,6,3,4,5,6});
+        System.out.println(test);
+        test = new Solution2().removeElements(test, 6);
+        System.out.println(test);
+    }
+}
