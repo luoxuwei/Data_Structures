35c5cf4e0463059a76a7cf6836f6d7174272e3a8
罗旭维 <luoxuwei@luoxuweideMacBook-Pro.local>
2019-09-18 22:31:12 +0800

    实现二分搜索树插入元素

--- BST/src/BST.java
+++ BST/src/BST.java
@@ -25,4 +25,21 @@ public class BST<E extends Comparable<E>> {
     public int size() {
         return size;
     }
+
+    public void add(E e) {
+        root = add(root, e);
+    }
+
+    private Node add(Node root, E e) {
+        if (root == null) {
+            size++;
+            return new Node(e);
+        }
+        if (root.e.compareTo(e) < 0) {
+            root.left = add(root.left, e);
+        } else if (root.e.compareTo(e) > 0) {
+            root.right = add(root.right, e);
+        }
+        return root;
+    }
 }
