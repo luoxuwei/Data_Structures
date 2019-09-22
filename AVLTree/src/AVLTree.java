208577a78a8dadf88b3887a48e5b4d160d9b8bfd
罗旭维 <luoxuwei@luoxuweideMacBook-Pro.local>
2019-09-22 16:08:36 +0800

    1.改造add 方法，使用递归实现，方便调整平衡操作 2.添加测试二分搜索和平衡性的测试代码

--- AVLTree/src/AVLTree.java
+++ AVLTree/src/AVLTree.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.List;
 
 public class AVLTree<K extends Comparable<K>, V> implements Map<K, V> {
 
@@ -20,34 +21,74 @@ public class AVLTree<K extends Comparable<K>, V> implements Map<K, V> {
     private int size;
     private Node root;
 
+    private int getHeight(Node node) {
+        if (node == null)
+            return 0;
+        return node.height;
+    }
+
+    private int getBlanceFactor(Node node) {
+        if (node == null)
+            return 0;
+        return getHeight(node.left) - getHeight(node.right);
+    }
+
     @Override
     public void add(K k, V v) {
+        root = add(root, k, v);
+    }
+
+    private Node add(Node root, K k, V v) {
         if (root == null) {
-            root = new Node(k, v);
-            return;
+            size++;
+            return new Node(k, v);
         }
-        Node parent = root;
-        Node cur = root;
-        while (cur != null && cur.k.compareTo(k) != 0) {
-            parent = cur;
-            if (cur.k.compareTo(k) < 0)
-                cur = cur.right;
-            else if (cur.k.compareTo(k) > 0)
-                cur = cur.left;
-        }
-
-        if (cur != null) {
-            cur.v = v;
-            return;
-        }
-        size++;
-
-        Node node = new Node(k, v);
-        if (parent.k.compareTo(k) > 0) {
-            parent.left = node;
+        if (root.k.compareTo(k)<0) {
+            root.right = add(root.right, k, v);
+        } else if (root.k.compareTo(k)>0) {
+            root.left = add(root.left, k,v);
         } else {
-            parent.right = node;
+            root.v = v;
         }
+        root.height = 1 + Math.max(getHeight(root.left), getHeight(root.right));
+        if (Math.abs(getBlanceFactor(root)) > 1)
+            System.out.println("unbalanced "+getBlanceFactor(root));
+
+        return root;
+    }
+
+    public boolean isBalance() {
+        return isBalance(root);
+    }
+
+    private boolean isBalance(Node node) {
+        if (node == null)
+            return true;
+
+        if (Math.abs(getBlanceFactor(node))>1)
+            return false;
+
+        return isBalance(node.left) && isBalance(node.right);
+    }
+
+    public boolean isBST() {
+        ArrayList<K> ks = new ArrayList<>();
+        inOrder(root, ks);
+        for (int i=1; i<ks.size(); i++) {
+            if (ks.get(i-1).compareTo(ks.get(i))>0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    private void inOrder(Node root, List<K> ks) {
+        if (root == null)
+            return;
+        inOrder(root.left, ks);
+        ks.add(root.k);
+        inOrder(root.right, ks);
     }
 
     @Override
@@ -172,6 +213,8 @@ public class AVLTree<K extends Comparable<K>, V> implements Map<K, V> {
             System.out.println("Total different words: " + map.getSize());
             System.out.println("Frequency of PRIDE: " + map.get("pride"));
             System.out.println("Frequency of PREJUDICE: " + map.get("prejudice"));
+            System.out.println("is balance: " + map.isBalance());
+            System.out.println("is bst: " + map.isBST());
         }
 
         System.out.println();
